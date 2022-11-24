1d221df45aca76a318448ddb78b1a441cc36ce32
Jean-Rene Menand <jrmemand@yahoo.com>
2022-11-23 19:11:19 -0800

    Changed simple text entry to a combo box, keeping track of the last N searches. Added tooltips to the search area buttons (warp, next, previous and close).
    search.go
      removed searchEntry, added searchBox and searchList.
      new functions getSearchEntry, newComboBox and appendSearchText
      updated function newSearchArea to use a comboBox instead and a text entry.
      updated function newSearchArea to use tooltips for wrap, next, previous and
      exit.
      updated setSearchFocus to get text entry from getSearchEntry amd to return
      that entry
      updated grapFocus to use the new setSearchFocus signature.
      updated findNext and findPrevious to call appendSearchText

--- search.go
+++ search.go
@@ -3,22 +3,96 @@ package main
 import (
     "log"
     "fmt"
-//    "strings"
-//    "os"
 	"github.com/gotk3/gotk3/gtk"
-//	"github.com/gotk3/gotk3/gdk"
+	"github.com/gotk3/gotk3/glib"
 )
 
 const (
     MAX_SELECTION_LENGTH = 63                   // in bytes
     MAX_TEXT_LENGTH = 2 * MAX_SELECTION_LENGTH  // in nibbles
+    MAX_STORE_ROW = 9                          // 10 entries  (0-9)
 )
 
 var (
     searchArea      *gtk.Box                // a search box
-    searchEntry     *gtk.Entry              // text entry
+    searchBox       *gtk.ComboBox           // combo box
+    searchList      *gtk.ListStore          // combo box store
     wrapMode        *gtk.ToggleButton       // search wrap 
 )
+
+func getSearchEntry( ) *gtk.Entry {
+    entry, err := searchBox.Bin.GetChild( )
+    if err != nil {
+        log.Fatalf( "getSearchTextEntry: unable to get entry child: %v\n", err )
+    }
+    return entry.(*gtk.Entry)
+}
+
+func newComboBox( maxInputLength int ) *gtk.ComboBox {
+    var err error
+    searchList, err = gtk.ListStoreNew( glib.TYPE_STRING )
+    if err != nil {
+        log.Fatalf( "newComboBox: cannot create ListStore: %v\n", err )
+    }
+    searchBox, err = gtk.ComboBoxNewWithModelAndEntry( searchList )
+    if err != nil {
+        log.Fatalf( "newComboBox: cannot create ComboBox: %v\n", err )
+    }
+    searchBox.SetEntryTextColumn(0)
+
+    entry := getSearchEntry()
+    entry.SetMaxLength( MAX_TEXT_LENGTH )
+    entry.SetCanFocus( true )
+    entry.Connect( "button-press-event", grabFocus )
+    entry.Connect( "key-press-event", hexFilter )
+    entry.Connect( "changed", incrementalSearch )
+
+    return searchBox
+}
+
+func appendSearchText( ) {
+    entry := getSearchEntry()
+    text, err := entry.GetText()
+    if err != nil {
+        panic("Cannot get entry text\n")
+    }
+
+    // if text already exists in list, just move its entry to the first entry
+    nEntries := 0
+    if iter, nonEmpty := searchList.GetIterFirst( ); nonEmpty {
+        for {
+            v, err := searchList.GetValue( iter, 0 )
+            if err != nil {
+                log.Fatalf( "appendSearchText: unable to get list value: %v\n",
+                            err )
+            }
+            var ls string
+            ls, err = v.GetString()
+            if err != nil {
+                log.Fatalf( "appendSearchText: unable to get list string: %v\n",
+                            err )
+            }
+            if ls == text {
+                searchList.MoveAfter( iter, nil )
+                return
+            }
+            nEntries ++
+            if false == searchList.IterNext( iter ) {
+                break
+            }
+        }
+    }
+    // otherwise check if there is room and just prepend the text
+    if nEntries >= MAX_STORE_ROW {             // remove last entry
+        path, _ := gtk.TreePathNewFromIndicesv( []int{ MAX_STORE_ROW } )
+        iter, _ := searchList.GetIter( path )
+        searchList.Remove( iter )
+    }
+    iter := searchList.InsertAfter( nil )       // first entry
+    if err := searchList.SetValue( iter, 0, text ); err != nil {
+        log.Fatalf( "appendSearchText: unable to get append item: %v\n", err )
+    }
+}
 
 // search area is a horizontal box with one label, one text input, two buttons
 // for next and previous. It uses the status area to indicate the number of 
@@ -31,33 +105,24 @@ func newSearchArea( ) *gtk.Box {
 
     label, err := gtk.LabelNew( localizeText( findPrompt ) )
     if err != nil {
-        log.Fatal("newSearchArea: could not create search prompt:", err)
+        log.Fatalf("newSearchArea: could not create search prompt: %v\n", err)
     }
     sa.PackStart( label, false, false, 0 )
 
-    entry, err := gtk.EntryNew( )
-    if err != nil {
-        log.Fatal("newSearchArea: could not create search entry:", err)
-    }
-    entry.SetMaxLength( MAX_TEXT_LENGTH )
-    entry.SetCanFocus( true )
-    entry.Connect( "button-press-event", grabFocus )
-    entry.Connect( "key-press-event", hexFilter )
-    entry.Connect( "changed", incrementalSearch )
-
-    searchEntry = entry
-    sa.PackStart( entry, true, true, 1 )
+    cb := newComboBox( MAX_TEXT_LENGTH )
+    sa.PackStart( cb, true, true, 1 )
 
     wrap, err := gtk.ToggleButtonNew( )
     if err != nil {
-        log.Fatal("newSearchArea: could not create wrap button:", err)
+        log.Fatalf("newSearchArea: could not create wrap button: %v\n", err)
     }
     wrapAround, err := gtk.ImageNewFromIconName(  "view-refresh", gtk.ICON_SIZE_BUTTON )
     if err != nil {
-        log.Fatal("newSearchArea: could not create wrapAround image:", err)
+        log.Fatalf("newSearchArea: could not create wrapAround image: %v\n", err)
     }
     wrap.SetImage( wrapAround )
     wrapMode = wrap
+    wrap.SetTooltipText( "Wrap around" )
     sa.PackStart( wrap, false, false, 0 )
 
     next, err := gtk.ButtonNewWithLabel( localizeText( buttonNext ) )
@@ -65,20 +130,23 @@ func newSearchArea( ) *gtk.Box {
         log.Fatal("newSearchArea: could not create next button:", err)
     }
     next.Connect( "button-press-event", findNext  )
+    next.SetTooltipText( "Next match" )
     sa.PackStart( next, false, false, 0 )
 
     previous, err := gtk.ButtonNewWithLabel( localizeText( buttonPrevious ) )
     if err != nil {
-        log.Fatal("newSearchArea: could not create previous button:", err)
+        log.Fatalf("newSearchArea: could not create previous button: %v\n", err)
     }
     previous.Connect( "button-press-event", findPrevious  )
+    previous.SetTooltipText( "Previous match" )
     sa.PackStart( previous, false, false, 0 )
 
     exit, err := gtk.ButtonNewFromIconName( "window-close", gtk.ICON_SIZE_BUTTON )
     if err != nil {
-        log.Fatal("newSearchArea: could not create exit button:", err)
+        log.Fatalf("newSearchArea: could not create exit button: %v\n", err)
     }
     exit.Connect( "button-press-event", hideSearchArea )
+    exit.SetTooltipText( "Close search" )
     sa.PackStart( exit, false, false, 0 )
 
 //    sa.SetCanFocus( true )
@@ -98,19 +166,26 @@ func hideSearchArea( ) {
 }
 
 func releaseSearchFocus( ) {
+/*
     searchEntry.SelectRegion( 0, 0 )
+*/
+    entry := getSearchEntry()
+    entry.SelectRegion( 0, 0 )
 }
 
-func setSearchFocus( ) {
+func setSearchFocus( ) *gtk.Entry {
     data := transferFocus( MAX_SELECTION_LENGTH )
     l := len(data)
+    entry := getSearchEntry()
+
     if l > 0 {
         b := make( []byte, l << 1 )
         writeHexDigitsFromSlice( b, data )
-        searchEntry.SetText( string(b) )
-        searchEntry.SetPosition( -1 )
+        entry.SetText( string(b) )
+        entry.SetPosition( -1 )
     }
-    searchEntry.GrabFocusWithoutSelecting()
+    entry.GrabFocusWithoutSelecting()
+    return entry
 }
 
 func grabFocus( entry *gtk.Entry ) bool {
@@ -137,9 +212,6 @@ func BytesFromHexString( l int, s string ) (res []byte) {
 }
 
 func findCurrentText( text string ) {
-    // TODO: disable nexr/previous if odd length, enable is even length
-    // use widget.SetSensitive( true/false )
-
     l := (len(text) >> 1) << 1
     pattern = BytesFromHexString( l, text )
     pc := getCurrentPageContext()
@@ -160,9 +232,8 @@ func search( ) {
     searchArea.Show( )
     removeHighlights()
 
-    setSearchFocus()
-
-    text, err := searchEntry.GetText()
+    entry := setSearchFocus()
+    text, err := entry.GetText()
     if err != nil {
         panic("Cannot get entry text\n")
     }
@@ -175,12 +246,14 @@ func search( ) {
 
 func findNext( button *gtk.Button ) bool {
 //    fmt.Printf( "Button Released on next!\n")
+    appendSearchText()
     selectNewMatch( true )
     return true
 }
 
 func findPrevious( button *gtk.Button ) bool {
 //    fmt.Printf( "Button Released on previous!\n")
+    appendSearchText()
     selectNewMatch( false )
     return true
 }
